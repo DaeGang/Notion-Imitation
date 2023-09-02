3198e837549e531e3c5a1d103aef7ebace98d3b5
yl0937 <yl1033371@gmail.com>
2023-09-02 12:48:44 +0900

    * feat : Initial Settings

---
+++ src/test/java/com/daegang/notion/NotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daegang.notion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
