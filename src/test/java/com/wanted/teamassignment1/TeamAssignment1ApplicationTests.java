de64fc1f19896f524fbbe593faf6e348b2d20599
김강연 <kkyxw@naver.com>
2023-09-02 19:42:08 +0900

    feat: 프로젝트 초기 틀 생성
    - 스프링 부트 버전: 2.7.15
    - Java 버전: 11
    - 의존성 목록:
      Spring Web
      Validation
      DevTools
      Lombok
      MySQL Driver

---
+++ src/test/java/com/wanted/teamassignment1/TeamAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.teamassignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamAssignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
